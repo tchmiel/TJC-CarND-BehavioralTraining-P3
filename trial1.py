714d4ee84b3735927a6a8c86e5327e94bac52cd8
tchmiel <tom.chmielenski@bentley.com>
2017-08-27 21:03:36 -0400

    change data path

--- trial1.py
+++ trial1.py
@@ -22,8 +22,8 @@ for line in lines:
 X_train = np.array(center_images)
 y_train = np.array(measurements)
 
-from keras.models = import Sequential
-from keras.layers = import Flatten, Dense
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
 
 model = Sequential()
 model.add(Flatten(input_shape=(160, 320, 3)))
