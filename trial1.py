2c31e599fb7c5f7e38b69d4c76ea2acb48499c88
tchmiel <tom.chmielenski@bentley.com>
2017-08-27 21:24:25 -0400

    wip: 107

--- trial1.py
+++ trial1.py
@@ -12,7 +12,7 @@ center_images=[]
 measurements = []
 for line in lines:
 		source_path = line[0]
-		filename = source_path.split('/')[-1]
+		filename = source_path.split('\\')[-1]
 		current_path = './data/P3Data_1/IMG/' + filename
 		print (current_path)
 		center_image = cv2.imread(current_path)
