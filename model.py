215242f9165c8ebf748e2c1fe7d55b419f7fb273
tchmiel <tom.chmielenski@bentley.com>
2017-09-07 00:27:44 +0000

    copy source code from jupyter notebook to model.py, and validated model.py ran successfully

--- model.py
+++ model.py
@@ -7,9 +7,9 @@ TEST_SIZE = 0.20
 BATCH_SIZE = 32
 NUM_EPOCHS = 3
 
-samples = []
-remove_zero_percentage = 0.95
 
+### load datasets
+remove_zero_percentage = 0.95
 def relative_image_path (root_data_dir, image_path):
     # converts driving_log.csv image path to be relative image path
     image_path = image_path.strip()
@@ -24,7 +24,8 @@ def relative_image_path (root_data_dir, image_path):
     return relative_path
     
 def load_dataset(root_data_dir):
-    global samples 
+    ds_samples = []
+    ds_images_removed = 0
     csvfilename = root_data_dir + 'driving_log.csv'
     with open(csvfilename) as csvfile:
         reader = csv.reader(csvfile)
@@ -34,12 +35,13 @@ def load_dataset(root_data_dir):
             center_angle = float(line[3])
             prob =  np.random.rand()
             if (abs(center_angle) < 0.01 and prob < remove_zero_percentage):
+                ds_images_removed += 1
                 continue
             line[0] = relative_image_path(root_data_dir, line[0])
             line[1] = relative_image_path(root_data_dir, line[1])
             line[2] = relative_image_path(root_data_dir, line[2])
-            samples.append(line)
-    return samples
+            ds_samples.append(line)
+    return ds_samples, ds_images_removed
 
 datasets = []
 datasets.append("./data/Udacity/")  #Udacity Training Set
@@ -47,13 +49,19 @@ datasets.append("./data/CenterLaneDriving/")
 datasets.append("./data/TeenagerDriver/")
 datasets.append("./data/CounterClockwise/")
 
+samples = []
+images_removed = 0
 for ds in datasets:
-    load_dataset(ds)
+    ds_samples, ds_images_removed =  load_dataset(ds)
+    samples.extend (ds_samples)
+    images_removed += ds_images_removed
 
-print("Number of samples =", len(samples))
+print("Number of total samples =", len(samples) + images_removed)
+print("Number of samples removed =", images_removed)
+print("Number of usable samples =", len(samples))
 
 
-
+ ###  Utiltiy Functions
 import os
 
 def ensure_dir(file_path):
@@ -67,13 +75,17 @@ def create_subDir (dir, sub_dir):
     return (new_path)
 
 
-
+### Image Plot utilities
 #importing some useful packages
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
+#%matplotlib inline
 
+
+
+### Image Methods
 
 def saveImage(imageFilename, image):  
     RGB_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -84,6 +96,7 @@ def displayImage(image):
         plt.imshow(image)
         plt.show()
 
+### Image Methods
 def flip_image (image, measurement):
     image_flipped = np.fliplr(image)
     measurement_flipped = -measurement
@@ -103,9 +116,13 @@ def grayscale(img):
     # Or use BGR2GRAY if you read an image with cv2.imread()
     # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+
+### Split off 20% of the samples to be a validation set
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size = TEST_SIZE)
 
+
+### generator
 import cv2
 import numpy as np
 import sklearn
@@ -149,10 +166,6 @@ def generator(samples, batch_size = BATCH_SIZE):
                 steering_right_angle = center_angle - steering_correction_factor
                 images.append(right_image)
                 angles.append(steering_right_angle)
-
-                right_image = cv2.imread(name)
-                right_image = colorCorrect_image(right_image)
-                steering_right_angle = center_angle - steering_correction_factor
                 
                 flipped_image, flipped_angle = flip_image (center_image, center_angle)
                 images.append(flipped_image)
@@ -168,45 +181,22 @@ train_generator = generator(train_samples, batch_size = BATCH_SIZE)
 validation_generator = generator(validation_samples, batch_size = BATCH_SIZE)
 
 
+
+## Model Architecture
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import Lambda, Cropping2D, Flatten, Dense, MaxPooling2D, Dropout
 
-# first attempt
-# Preprocess incoming data, centered around zero with small standard deviation 
-#model = Sequential()
-#model.add(Lambda(lambda x: ((x / 255.0) - 0.5), input_shape=(160,320,3))) #normalize the data
-#model.add(Flatten())
-#model.add(Dense(1))
-
-#model.summary()
-
-# second attempt - LeNet
-#model = Sequential()
-#model.add(Lambda(lambda x: ((x / 255.0) - 0.5), input_shape=(160,320,3))) #normalize the data
-#model.add(Cropping2D(cropping = ((55,25), (0,0))))  # ((PixelsFromTop. FromBottom), (FromLeft,FromRight))
-#model.add(Convolution2D(6,5,5, activation = "relu"))
-#model.add(MaxPooling2D())
-#model.add(Convolution2D(16,5,5, activation = "relu"))
-#model.add(MaxPooling2D())
-#model.add(Flatten())
-#model.add(Dense(120))
-#model.add(Dropout(0.5))
-#model.add(Dense(84))
-#model.add(Dropout(0.5))
-#model.add(Dense(1))
-
-# print out model summary
 
 # third attempt - NVidia
 model = Sequential()
 model.add(Lambda(lambda x: ((x / 255.0) - 0.5), input_shape = (160,320,3))) #normalize the data
 model.add(Cropping2D(cropping = ((65,25), (0,0))))  # ((PixelsFromTop. FromBottom), (FromLeft,FromRight))
-model.add(Convolution2D(24,5,5, border_mode = "valid", subsample = (2,2), activation = 'relu'))
-model.add(Convolution2D(36,5,5, border_mode = "valid", subsample = (2,2), activation = 'relu'))
-model.add(Convolution2D(48,5,5, border_mode = "valid", subsample = (2,2), activation ='relu'))
-model.add(Convolution2D(64,3,3, activation = 'relu'))
-model.add(Convolution2D(64,3,3, activation = 'relu'))
+model.add(Convolution2D(24,5,5, border_mode = "valid", subsample = (2,2), activation = 'elu'))
+model.add(Convolution2D(36,5,5, border_mode = "valid", subsample = (2,2), activation = 'elu'))
+model.add(Convolution2D(48,5,5, border_mode = "valid", subsample = (2,2), activation ='elu'))
+model.add(Convolution2D(64,3,3, activation = 'elu'))
+model.add(Convolution2D(64,3,3, activation = 'elu'))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dropout(0.5))
@@ -215,7 +205,6 @@ model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Dropout(0.5))
 model.add(Dense(1))
-
 
 
 # print out model summary
@@ -228,5 +217,5 @@ history_object = model.fit_generator(train_generator,
             nb_val_samples = len(validation_samples), 
             nb_epoch = NUM_EPOCHS, verbose  = 2)
 
-model.save('model_20170903_both_nvidia.h5')
+model.save('model_final.h5')
 
